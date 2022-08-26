08658cc514aa519ad999a0e66227b6ff60ea6d61
knud21kea <grah0070@stud.kea.dk>
2022-08-26 09:00:54 +0200

    first commit

---
+++ src/main/java/com/example/hellow/HelloWApplication.java
@@ -0,0 +1,15 @@
+package com.example.hellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HelloWApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellow/HelloWApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.hellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
